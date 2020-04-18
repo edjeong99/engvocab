d514fda8f6fd35d8f8a1d20b87479d1ca24c5ac4
Edward J <edjeong99@gmail.com>
2020-04-18 16:04:30 -0500

    working on quiz component

--- src/data/wordList.js
+++ src/data/wordList.js
@@ -1,18 +1,18 @@
 var wordList = [
-  { word: 'Home', part: 'noun', description: 'place to live' },
+  { word: 'home', part: 'noun', description: 'place to live' },
   {
-    word: 'Apple',
+    word: 'apple',
     part: 'noun',
     description:
       ' the round fruit of a tree of the rose family, which typically has thin red or green skin and crisp flesh.',
   },
   {
-    word: 'Happy',
+    word: 'happy',
     part: 'adjective',
     description: 'feeling or showing pleasure or contentment.',
   },
   {
-    word: 'Sky',
+    word: 'sky',
     part: 'noun',
     description:
       'the region of the atmosphere and outer space seen from the earth',
